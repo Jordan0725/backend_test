9defa390598421ca864abc08791828ef1447dac1
Luis Tanese <luistanese@gmail.com>
2025-03-19 18:56:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,15 +12,18 @@ let contador = 0;
 app.get("/api", (req, res) => {
     const { incremento } = req.query;
 
-
     contador += parseInt(incremento);
 
     console.log("contador: " + contador);
     res.status(200).send("adicionado");
 });
 
+app.get("/api/contador", (req, res) => {
+    res.status(200).send("Contador: " + contador);
+});
+
 app.get("/api/teste", (req, res) => {
     res.status(200).send("API Funcionando perfeitamente!!!!!");
-});//
+});
 
 module.exports = app;
